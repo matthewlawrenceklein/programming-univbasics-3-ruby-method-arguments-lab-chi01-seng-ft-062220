2ae4c29cce748b33e32402709580bb9af05f9464
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-18 23:30:18 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,7 @@ def introduction(name)
 end
 
 
-def introduction_with_language(name, langauge)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
